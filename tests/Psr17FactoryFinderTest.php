3f23f879f0ac2e610e89e7b72f0b57b74912168b
ncou <nicolas_courade@yahoo.fr>
2022-05-11 22:58:43 +0200

    update

--- tests/Psr17FactoryFinderTest.php
+++ tests/Psr17FactoryFinderTest.php
@@ -4,8 +4,43 @@ declare(strict_types=1);
 
 namespace Http\Factory;
 
+use RuntimeException;
+
 class Psr17FactoryFinderTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @dataProvider getFactories
+     */
+    public function testFind($method, $interface)
+    {
+        $callable = [Psr17FactoryFinder::class, $method];
+        $client = $callable();
+        $this->assertInstanceOf($interface, $client);
+    }
+
+    /**
+     * @group NothingInstalled
+     * @dataProvider getFactories
+     */
+    public function testNotFound($method)
+    {
+        $callable = [Psr17FactoryFinder::class, $method];
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('No PSR-17 implementations found for');
+        $callable();
+    }
+
+    public function getFactories()
+    {
+        yield ['findRequestFactory', RequestFactoryInterface::class];
+        yield ['findResponseFactory', ResponseFactoryInterface::class];
+        yield ['findServerRequestFactory', ServerRequestFactoryInterface::class];
+        yield ['findStreamFactory', StreamFactoryInterface::class];
+        yield ['findUploadedFileFactory', UploadedFileFactoryInterface::class];
+        yield ['findUriFactory', UriFactoryInterface::class];
+    }
+
+    /*
     public function testCanFindRequestFactory(): void
     {
         static::assertEquals(Psr17FactoryFinder::findRequestFactory(), Psr17FactoryFinder::findRequestFactory());
@@ -34,5 +69,5 @@ class Psr17FactoryFinderTest extends \PHPUnit\Framework\TestCase
     public function testCanFindUriFactory(): void
     {
         static::assertEquals(Psr17FactoryFinder::findUriFactory(), Psr17FactoryFinder::findUriFactory());
-    }
+    }*/
 }
